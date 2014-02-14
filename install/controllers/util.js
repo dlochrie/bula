22e810ead63fc337c63a18f91924d969102e9357
Daniel Lochrie <dlochrie@users.noreply.github.com>
2014-02-13 20:40:22 -0600

    Added util method for date.

--- install/controllers/util.js
+++ install/controllers/util.js
@@ -39,6 +39,22 @@ Util.getDate = function() {
 
 
 /**
+ * Converts the date to 'Month Day, Year HH:MM' format.
+ * @param {string} date The date string to format.
+ * @return New date object
+ */
+Util.getHumanDate = function(date) {
+  return new Date(date).toLocaleTimeString('en-US', {
+    month: 'long',
+    day: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit'
+  });
+};
+
+
+/**
  * See this link for some crazy awesome HTML sanitization:
  * https://code.google.com/p/google-caja/source/browse/trunk/src/com/
  * --> google/caja/plugin/html-sanitizer.js
