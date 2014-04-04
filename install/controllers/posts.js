6238cbae3fba77faf7a182ace0802c2fad35042e
Daniel Lochrie <dlochrie@users.noreply.github.com>
2014-04-03 20:57:54 -0500

    Updated Posts index page to show posts lists, and updated the controller to match prototypical style.

--- install/controllers/posts.js
+++ install/controllers/posts.js
@@ -1,18 +1,53 @@
 var Post = require('../models/post');
 
 
-exports.index = function(req, res) {
-  var post = new Post(req.app, null);
-  post.find(null, function(err, posts) {
-    if (err) {
-      // TODO: Bad way of handling error. Should flash...
-      res.send('There was an error getting posts', err);
+/**
+ * Expose `Posts` Controller.
+ */
+module.exports = new Posts;
+
+
+
+/**
+ * Posts Controller.
+ * @constructor
+ */
+function Posts() {}
+
+
+/**
+ * Path to posts index page.
+ * @private {string}
+ * @const
+ */
+Posts.INDEX_VIEW_ = 'posts/';
+
+
+/**
+ * Path to posts show view.
+ * @private {string}
+ * @const
+ */
+Posts.SHOW_VIEW_ = 'posts/show';
+
+
+/**
+ * Renders posts' index page - lists all posts.
+ * @param {http.IncomingMessage} req Node/Express request object.
+ * @param {http.ServerResponse} res Node/Express response object.
+ */
+Posts.prototype.index = function(req, res) {
+  var params = req.body || {};
+  var post = new Post(req.app, params);
+  post.find(function(err, results) {
+    if (err || !results) {
+      req.flash('error', 'There was an error getting the posts: ' + err);
+      res.redirect('/');
     } else {
-      console.log(posts);
       res.render('posts/index', {
-        description: 'Browse the Latest Posts.',
-        posts: posts,
-        title: 'Blog Posts'
+        title: 'Latest Posts',
+        description: 'Browse the latest posts.',
+        results: results
       });
     }
   });
