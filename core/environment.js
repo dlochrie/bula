0b412be32e6ea6a86f69961268994668f86789f7
Daniel Lochrie <dlochrie@users.noreply.github.com>
2014-04-12 15:42:43 -0500

    Did some more moving of middleware to the core middleware module.

--- core/environment.js
+++ core/environment.js
@@ -2,66 +2,7 @@
  * @param {function(Object, Object, Function)} app Express application instance.
  */
 module.exports = function(app) {
-  var flash = require('connect-flash'),
-      express = require('express'),
-      path = require('path'),
-      passport = require('passport'),
-      RedisStore = require('connect-redis')(express),
-      rootPath = app.get('ROOT PATH');
-
-  /**
-   * Compress Pages and Assets for speed/performance.
-   */
-  app.use(express.compress());
-  app.use(express.static(path.join(rootPath, 'public')));
+  var rootPath = app.get('ROOT PATH');
   app.set('views', rootPath + '/app/views');
   app.set('view engine', 'jade');
-
-  /**
-   * Set up  Body Parsing, omitting the Multipart middleware.
-   * For more information, see: http://expressjs.com/api.html#bodyParser.
-   */
-  app.use(express.json());
-  app.use(express.urlencoded());
-
-  // Allows us to use "Put", "Delete", and misc request verbs.
-  app.use(express.methodOverride());
-
-  /**
-   * Setup Cookies and Session.
-   */
-  app.use(express.cookieParser(app.get('COOKIE SECRET')));
-  app.use(express.session({
-    store: new RedisStore(),
-    secret: app.get('REDIS SECRET')
-  }));
-
-  /**
-   * Setup Passport Configuration.
-   */
-  app.use(passport.initialize());
-  app.use(passport.session());
-
-  /**
-   * Expose Flash Middleware to Views.
-   * TODO: This should be abstracted out.
-   */
-  app.use(flash());
-  app.use(function(req, res, next) {
-    res.locals.messages = req.flash();
-    next();
-  });
-
-  /**
-   * Make the User's Name available to Views
-   * TODO: Is this necessary every request???
-   */
-  app.use(function(req, res, next) {
-    if (req.session.logged_in) {
-      res.locals.user = req.session.passport.user;
-    }
-    next();
-  });
-
-  app.use(app.router);
 };
--- core/middleware.js
+++ core/middleware.js
@@ -1,30 +1,58 @@
 /**
- * Base Middleware Module
+ * Base Middleware Module.
+ * This is where core middleware is registered.
+ * CAUTION: Order is extremely important here!!! Be careful when changing the
+ * order of the middleware.
  * @param {function(Object, Object, Function)} app Express application instance.
  */
 module.exports = function(app) {
-  /**
-   * Make the User's Name available to Views
-   */
-  app.use(function(req, res, next) {
-    if (req.session.logged_in) {
-      res.locals.user = req.session.passport.user;
-    }
+  var express = require('express'),
+      flash = require('connect-flash'),
+      passport = require('passport'),
+      path = require('path'),
+      rootPath = app.get('ROOT PATH'),
+      RedisStore = require('connect-redis')(express);
 
-    // TODO: See if these are even if use - if so, FOMALIZE THEM.
-    res.locals.title =
-        'A nice title here!';
-    res.locals.description =
-        'A nice desciption here.';
-    res.locals.canonical = app.get('ROOT URL').replace(/\/$/, '') + req.url;
-    next();
-  });
+  // Compress Pages and Assets for speed/performance.
+  app.use(express.compress());
+  app.use(express.static(path.join(rootPath, 'public')));
 
-  /**
-   * Expose Flash Middleware to Views.
-   */
-  app.use(function(req, res, next) {
+  // Set up  Body Parsing, omitting the Multipart middleware.
+  // For more information, see: http://expressjs.com/api.html#bodyParser.
+  app.use(express.json());
+  app.use(express.urlencoded());
+
+  // Allows us to use "Put", "Delete", and misc request verbs.
+  app.use(express.methodOverride());
+
+  // Setup Cookies and Session.
+  app.use(express.cookieParser(app.get('COOKIE SECRET')));
+  app.use(express.session({
+    store: new RedisStore(),
+    secret: app.get('REDIS SECRET')
+  }));
+
+  // Setup Passport Configuration.
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  // Exposes the Flash Middleware to Views.
+  app.use(flash());
+  app.use(function loadFlash(req, res, next) {
     res.locals.messages = req.flash();
     next();
   });
+
+  // Makes the User's Name available to Views.
+  app.use(function loadUser(req, res, next) {
+    if (req.session.logged_in) {
+      res.locals.user = req.session.passport.user;
+    }
+    next();
+  });
+
+  // Exposes the router. This should ALWAYS BE LOADED LAST to prevent issues
+  // with flash messages, and session storage/retrieval, in addition to other
+  // things.
+  app.use(app.router);
 };
