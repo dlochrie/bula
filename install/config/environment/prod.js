cd79283700ae064b194ae90257ca23a645f7abae
Daniel Lochrie <dlochrie@users.noreply.github.com>
2014-04-12 16:28:17 -0500

    Added (for real) the prod environment conf.

---
+++ install/config/environment/prod.js
@@ -0,0 +1,18 @@
+/**
+ * Production environment-specific settings and middleware. Modify this file as
+ * necessary to override the core/middleware file or to append new middleware.
+ *
+ * For example, the default view engine is `jade`, but you can override that
+ * setting here by adding:
+ *
+ *     app.set('view engine', 'ejs');
+ *
+ * If you wanted to use express logger middleware, you could register here like:
+ *
+ *     app.use(express.logger());
+ *
+ * @param {function(Object, Object, Function)} app Express application instance.
+ * @param {Function} express Express/Connect instance.
+ */
+module.exports = function(app, express) {
+};
