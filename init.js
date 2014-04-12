0bca6c8947b1151ca8d299e8acd6b555f2fb148e
Daniel Lochrie <dlochrie@users.noreply.github.com>
2014-04-12 16:04:49 -0500

    Moved core/default settings to the core/environment file.

--- init.js
+++ init.js
@@ -12,51 +12,21 @@ module.exports = function(app, express) {
   var dir = __dirname + '/core/',
       fs = require('fs');
 
-  /**
-   * Set up site globals - these are based off of Environmental variables.
-   */
-  var env = (process.env.NODE_ENV || 'dev').toUpperCase();
-  app.set('NODE ENVIRONMENT', env);
-  app.set('NODE PORT', process.env[env + '_NODE_PORT'] || 3000);
-  app.set('NODE HOST', process.env[env + '_NODE_HOST'] || '0.0.0.0');
-  app.set('ROOT PATH', process.env[env + '_ROOT_PATH']);
-  app.set('ROOT URL', process.env[env + '_ROOT_URL']);
-  app.set('MYSQL DB', process.env[env + '_MYSQL_DB']);
-  app.set('MYSQL HOST', process.env[env + '_MYSQL_HOST']);
-  app.set('MYSQL USER', process.env[env + '_MYSQL_USER']);
-  app.set('MYSQL PASS', process.env[env + '_MYSQL_PASS']);
-  app.set('MYSQL MAX CONN', process.env[env + '_MYSQL_MAX_CONN']);
-  app.set('COOKIE SECRET', process.env[env + '_COOKIE_SECRET']);
-  app.set('REDIS SECRET', process.env[env + '_REDIS_SECRET']);
-
-  /**
-   * Extract the Site Owners and store as global.
-   */
-  var owners = process.env[env + '_SITE_OWNERS'];
-  var ownersList = owners ? owners.split(',') : [];
-  var siteOwners = ownersList.map(function(owner) {
-    // TODO: Maybe also verify if this is an email address or not.
-    return owner.trim();
-  });
-  app.set('SITE OWNERS', siteOwners);
-
-  /**
-   * Load all required core files.
-   */
-  var modules = fs.readdirSync(dir);
+  // Load all required core files. Order is important.
+  var modules = [
+    'environment',
+    'middleware'
+  ];
   modules.forEach(function(module) {
     require(dir + '/' + module)(app);
   });
 
-  /**
-   * Extract environmental variables.
-   */
+  // Extract environmental variables.
   var root = app.get('ROOT PATH'),
       env = app.get('NODE ENVIRONMENT');
 
-  /**
-   * Load application-specific config files (routes, etc).
-   */
+  // Load application-specific config files (routes, etc) from the `app/config`
+  // directory. Any overrides, or custom settings/middleware should come from here.
   var confs = fs.readdirSync(root + 'config');
   confs.forEach(function(conf) {
     var path = root + 'config/' + conf;
@@ -77,8 +47,6 @@ module.exports = function(app, express) {
     require(conf)(app, express);
   }
 
-  /**
-   * Always load the Routes last.
-   */
+  // Always load the Routes last.
   require(root + 'config/routes.js')(app);
 };
