61ed31a96955e71b904afddd2031a5a48b4b9b6c
Daniel Lochrie <dclochri@gmail.com>
2014-07-02 08:37:07 -0500

    Fixed initialization of bula-test in bula apps.

--- install/test/setup.js
+++ install/test/setup.js
@@ -4,10 +4,6 @@ process.env.NODE_ENV = 'test';
 // Make sure the global `app` is available to tests.
 app = this.app || require('../app');
 
-
-/**
- * Attach the `bula-test` module to the global `app`. It contains utilities for
- * seeding fixtures and mocking authentication/sessions in tests.
- */
-var Test = require('bula-test');
-var test = new Test(app);
+// Attach the `bula-test` module to the global `app`. `bula-test` contains
+// utilities for seeding fixtures and mocking authentication/sessions in tests.
+require('bula-test')(app);
