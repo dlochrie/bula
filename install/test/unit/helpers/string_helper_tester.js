d878ce0c07e3c4d2885907cd8b0aba23dfe29f09
Daniel Lochrie <dclochri@gmail.com>
2014-07-03 18:26:20 -0500

    Starting to add tests for the String Helper.

---
+++ install/test/unit/helpers/string_helper_tester.js
@@ -0,0 +1,18 @@
+var StringHelper = require('../../../app/helpers/string');
+
+describe('String Helper', function() {
+  it('should convert markdown text to html', function(done) {
+    var testString = '# Header\n * item 1\n * item 2\n * item 3';
+    StringHelper.convertMarkdown(testString).should.eql(
+        '<h1 id=\"header\">Header</h1>\n<ul>\n<li>item 1</li>\n' +
+        '<li>item 2</li>\n<li>item 3</li>\n</ul>\n');
+    var testString2 = '[Go to Google](www.google.com)';
+    StringHelper.convertMarkdown(testString2).should.eql(
+        '<p><a href=\"www.google.com\">Go to Google</a></p>\n');
+    var testString3 = '`code`\n\n    var foo = "bar";';
+    StringHelper.convertMarkdown(testString3).should.eql(
+        '<p><code>code</code></p>\n<pre><code>var foo = &quot;bar&quot;;\n' +
+        '</code></pre>');
+    done();
+  });
+});
